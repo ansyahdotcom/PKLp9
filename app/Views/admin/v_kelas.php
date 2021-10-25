b41ea0838b3a14f44f539f28f379a811f41b2145
Rendy Wisnu R. / E41181766 <rendyrahardjo21@gmail.com>
2021-10-25 14:59:33 +0700

    Insert Data Kelas
    Berhasil insert tapi masih eror di unique id_kelas

--- app/Views/admin/v_kelas.php
+++ app/Views/admin/v_kelas.php
@@ -9,17 +9,18 @@
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <a href="#" class="btn btn-success btn-sm">
+            <a href="/kelas/addKelas" class="btn btn-success btn-sm">
                 <i class="fas fa-plus"></i>
-                Add data
+                Add Data
             </a>
-    </div>
+        </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>No</th>
+                            <th>ID Kelas</th>
                             <th>Nama Kelas</th>
                             <th>Jumlah Siswa</th>
                             <th>Last Update</th>
@@ -29,6 +30,7 @@
                     <tfoot>
                         <tr>
                             <th>No</th>
+                            <th>ID Kelas</th>
                             <th>Nama Kelas</th>
                             <th>Jumlah Siswa</th>
                             <th>Last Update</th>
@@ -36,22 +38,20 @@
                         </tr>
                     </tfoot>
                     <tbody>
-                        <?php foreach($kelas as $k) : ?>
-                        <?php $no = 1; ?>
-                        <tr>
-                            <td><?= $no++; ?></td>
-                            <td><?= $k['nama_kelas']; ?></td>
-                            <td><?= $jm_siswa; ?></td>
-                            <td><?= $k['updated_at']; ?></td>
-                            <td class="d-flex">
-                                <a href="#" class="btn btn-info btn-sm mr-2" title="lihat detail"><i
-                                        class="fas fa-eye"></i></a>
-                                <a href="#" class="btn btn-primary btn-sm mr-2" title="edit data"><i
-                                        class="fas fa-edit"></i></a>
-                                <a href="#" class="btn btn-danger btn-sm mr-2" title="hapus data"><i
-                                        class="fas fa-trash"></i></a>
-                            </td>
-                        </tr>
+                        <?php foreach ($kelas as $k) : ?>
+                            <?php $no = 1; ?>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $k['id_kelas']; ?></td>
+                                <td><?= $k['nama_kelas']; ?></td>
+                                <td><?= $jm_siswa; ?></td>
+                                <td><?= $k['updated_at']; ?></td>
+                                <td class="d-flex">
+                                    <a href="#" class="btn btn-info btn-sm mr-2" title="lihat detail"><i class="fas fa-eye"></i></a>
+                                    <a href="#" class="btn btn-primary btn-sm mr-2" title="edit data"><i class="fas fa-edit"></i></a>
+                                    <a href="#" class="btn btn-danger btn-sm mr-2" title="hapus data"><i class="fas fa-trash"></i></a>
+                                </td>
+                            </tr>
                         <?php endforeach; ?>
                     </tbody>
                 </table>
