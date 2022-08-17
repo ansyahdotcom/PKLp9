3f73b709adb88257a657d8b1c0e5426638a460cc
Bayu Agil Prananda <bayuagil04@gmail.com>
2022-08-17 14:30:12 +0700

    update tampilan perolehan suara kandidat

--- pemilos_core/app/Views/user/v_landing.php
+++ pemilos_core/app/Views/user/v_landing.php
@@ -19,51 +19,56 @@
                 <!-- Card Body -->
                 <div class="card-body">
                     <p class="font-italic text-gray-800 font-weight-bold"> Jika ingin melakukan voting silahkan klik menu voting ketua osis <sup class="text-danger">(*)</sup></p>
-                    <div class="col">
+                    <div class="row">
                         <?php foreach ($ketua as $ket) :
                             $id_kandidat = $ket['id_kandidat']; ?>
                             <?php
                             $db = \Config\Database::connect();
+                            // Prosentase Suara
+                            $jml_vote = $db->query("SELECT COUNT(id_kandidat) AS pembilang FROM voting WHERE id_kandidat = $id_kandidat")->getResultArray();
+                            foreach ($jml_vote as $pemb) {
+                                foreach ($vote as $vot) {
+                                    $penyebut = $vot['voting'];
+                                    $pembilang = $pemb['pembilang'];
+                                    if ($pembilang == 0 && $penyebut == 0) {
+                                        $hasil = 0;
+                                    } else {
+                                        $hasil = $pembilang / $penyebut * 100 + 0;
+                                    }
+                                    $prosentase = round($hasil, 2) . '%';
+                                }
+                            }
+
+                            // Jumlah Suara
+                            $jml_suara = $db->query("SELECT COUNT(id_kandidat) AS suara FROM voting WHERE id_kandidat = $id_kandidat")->getRow();
+
+                            // Menampilkan Nama Ketua dan Wakil ketua OSIS
                             $sql = $db->query("SELECT kandidat.wakil, user.nama_usr as wnama, kelas.nama_kelas as wkelas 
                                                                 FROM kandidat, user, kelas WHERE kandidat.wakil = user.nis 
                                                                 AND user.id_kelas = kelas.id_kelas AND kandidat.id_kandidat = $id_kandidat");
                             foreach ($sql->getResultArray() as $wakil) :
                             ?>
-                                <div class="card border-left-primary h-100 py-2">
-                                    <div class="card-navbar font-weight-bold">
-                                        <div class="row no-gutters align-items-center">
-                                            <div class="col mx-2">
-                                                <?= $ket['nama_pasangan'] . ' | ' . $ket['nama_usr'] . ' & ' . $wakil['wnama']; ?>
+                                <div class="col-md-4">
+                                    <div class="card bg-primary mb-2">
+                                        <div class="card-body">
+                                            <h1 class="font-weight-bolder text-center text-white"><?= $prosentase; ?></h1>
+                                            <p class="text-center text-white"><?= $jml_suara->suara; ?> suara</p>
+                                            <div class="progress progress-sm mr-2">
+                                                <div class="progress-bar bg-success" role="progressbar" style="width: <?= $hasil ?>%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card" style="height: fit-content;">
+                                        <img src="/assets/img/fotokandidat/<?= $ket['foto']; ?>" class="card-img-top" alt="img-paslon" style="object-fit: cover !important; height: 200px !important;">
+                                        <div class="card-body text-center">
+                                            <h5 class="font-weight-bolder text-dark"><?= $ket['nama_pasangan']; ?></h5>
+                                            <h6 class="font-weight-bold"><?= $ket['nama_usr'] . ' & ' . $wakil['wnama']; ?></h6>
+                                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
                                         </div>
                                     </div>
                                 </div>
                             <?php endforeach; ?>
-                            <br>
-                            <h4 class="small font-weight-bold">Perolehan Suara
-                                <div class="float-right mr-2">
-                                    <?php
-                                    $db = \Config\Database::connect();
-                                    $sqll = $db->query("SELECT COUNT(id_kandidat) AS pembilang FROM voting WHERE id_kandidat = $id_kandidat");
-                                    foreach ($sqll->getResultArray() as $pemb) {
-                                        foreach ($vote as $vot) {
-                                            $penyebut = $vot['voting'];
-                                            $pembilang = $pemb['pembilang'];
-                                            if ($pembilang == 0 && $penyebut == 0) {
-                                                $hasil = 0;
-                                            } else {
-                                                $hasil = $pembilang / $penyebut * 100 + 0;
-                                            }
-                                            echo round($hasil, 2) . '%';
-                                        }
-                                    }
-                                    ?>
-                                </div>
-                            </h4>
-                            <div class="progress progress-sm mr-2">
-                                <div class="progress-bar bg-success" role="progressbar" style="width: <?= $hasil ?>%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <hr>
                         <?php endforeach; ?>
                     </div>
                 </div>
